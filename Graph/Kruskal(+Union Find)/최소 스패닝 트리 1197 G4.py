084a01423bcfbe63ab400580985ff0150dcbb3bf
Jung Moongyu <jmoongyu24@gmail.com>
2025-09-07 17:04:48 +0900

    fix: 1197 comment

--- Graph/Kruskal(+Union Find)/최소 스패닝 트리 1197 G4.py
+++ Graph/Kruskal(+Union Find)/최소 스패닝 트리 1197 G4.py
@@ -15,10 +15,8 @@ def union_parent(a, b):
 def find_parent(x):
     if parent[x] != x:
         parent[x] = find_parent(parent[x])  # 경로 압축
-        #return find_parent(parent[x]) - 재귀적으로 탐색하지만, 부모의 값을 갱신하지 않으므로 트리의 높이가 커짐
     return parent[x]
-    # return x
-    
+
 V, E = map(int, input().split())
 
 parent = [i for i in range(V+1)]
