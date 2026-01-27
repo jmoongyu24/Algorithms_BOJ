674902fd749ceb51b83b409b1aa7a2955bfd5df8
Jung Moongyu <jmoongyu24@gmail.com>
2026-01-27 14:42:16 +0900

    9095, 2579, 11047 DPs, Greedy

---
+++ DP/계단 오르기 2579 S3.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+stair = [0]
+
+for _ in range(N):
+    stair.append(int(input()))
+
+dp = [0]*(N+1)
+
+if N < 3:
+    print(sum(stair))
+else:
+    dp[0] = stair[0]
+    dp[1] = stair[1]
+    dp[2] = stair[1] + stair[2]
+
+    for i in range(3, N+1):
+        dp[i] = max(dp[i-2], dp[i-3] + stair[i-1]) + stair[i]
+
+    print(dp[N])
--- Greedy/동전0 11047 S4.py
+++ Greedy/동전0 11047 S4.py
@@ -9,11 +9,9 @@ result = 0
 for _ in range(N):
     coin.append(int(input()))
 
-# 역순 순차 탐색을 위한 동전 정렬
 coin.sort(reverse = True)
 
 for c in coin:
-    # 더 이상 만들 수 있는 금액이 없으면 break
     if K == 0:
         break
     # 현재 금액에서 사용할수 있는 최대 동전 개수  K//c
